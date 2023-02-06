8a69479382cf4ef16e4b216304e52065d42ae1f9
small1-rohan <121851542+small1-rohan@users.noreply.github.com>
2023-02-06 23:16:11 +0600

    Create js_code.js

---
+++ js_code.js
@@ -0,0 +1,47 @@
+//                                           First Problem
+
+function mindGame(input) {
+  let multiply = input * 3;
+  let add = multiply + 10;
+  let divide = add / 2;
+  let subtract = divide - 5;
+  return subtract;
+}
+
+//                                          second problem
+
+function evenOdd(input) {
+  let len = input.length;
+  if (len % 2 !== 0) {
+    return "odd";
+  } else {
+    return "even";
+  }
+}
+
+//                                           third problem
+
+function isLGSeven(input) {
+  let subtract = input - 7;
+
+  if (subtract < 7) {
+    return subtract;
+  } else {
+    let output = subtract * 2;
+    return output;
+  }
+}
+
+//                                           forth problem
+
+
+
+
+
+
+
+
+
+
+
+//                                           fifth problem
